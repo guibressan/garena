4ccfa5e1310bc8b2989a0bfa36c2a5d8695a16f1
guibressan <g.br3ssan@gmail.com>
2025-08-02 00:14:06 +0000

    * Added test examples showing what not to do

--- arena_test.go
+++ arena_test.go
@@ -3,6 +3,7 @@ package garena
 import (
 	"bytes"
 	"runtime"
+	"runtime/debug"
 	"testing"
 	"unsafe"
 )
@@ -70,6 +71,73 @@ func Test_ptrAlign(t *testing.T) {
 			assert(v.expected == ptrAlign(v.ptr, v.align))
 		})
 	}
+}
+
+func Test_Segfault(t *testing.T) {
+	var (
+		a     *Arena
+		value *byte
+	)
+
+	a = New(1)
+
+	value = Alloc[byte](a)
+	*value = 0xCC
+	assert(0xCC == *value)
+
+	a.FreeAll()
+
+	// use after free is undefined behavior, for now the memory is zeroed,
+	// but you can end up changing another allocated value
+	assert(0x00 == *value)
+
+	a.Destroy()
+
+	// uncomment to get a segfault
+	// assert(0x00 == *value)
+}
+
+func Test_MaySegfaultOrUB(t *testing.T) {
+	type testData struct {
+		data []byte
+	}
+	var (
+		a  *Arena
+		td *testData
+	)
+
+	a = New(1)
+	defer a.Destroy()
+
+	f := func(a *Arena, td **testData) {
+		*td = Alloc[testData](a)
+		// There's no guarantee that the GC won't free the allocated
+		// bytes, since we are allocating outside Go heap
+		(*td).data = make([]byte, 100<<20)
+		for i := range (*td).data {
+			(*td).data[i] = 0xCC
+		}
+	}
+
+	maySegfaultOrUB := func() {
+		for i := range 1_000 {
+			f(a, &td)
+			runtime.GC()
+			debug.FreeOSMemory()
+			t.Log(i)
+			// MAY segfault
+			assert(td.data[0] == 0xCC)
+			a.FreeAll()
+		}
+	}
+
+	debug.SetGCPercent(1)
+	debug.SetMemoryLimit(1 << 20)
+
+	// uncomment to maybe crash the program
+	// maySegfaultOrUB()
+	_ = maySegfaultOrUB
+
 }
 
 func BenchmarkArenaAlloc(b *testing.B) {
